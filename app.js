7614b1a25a06866d95927f3156ecef12ad9d478f
David <david.robles91@gmail.com>
2023-09-20 10:47:23 -0600

    Adding main fike and side file to test dynamic loading

---
+++ app.js
@@ -0,0 +1,3 @@
+const hotReloadFunction = require('./hotReloadModule');
+
+hotReloadFunction();
---
+++ hotReloadModule.js
@@ -0,0 +1,5 @@
+function hotReloadFunction() {
+    console.log("myFunction in myFunctionModule has been executed!");
+}
+
+module.exports = hotReloadFunction;
